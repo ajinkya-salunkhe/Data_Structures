ae62d0268fd721816568de5ea9c8a0c5cf930bfa
ajinkya-salunke <ajinkyasalunke1998@gmail.com>
2021-04-09 01:44:47 -0700

    Delete fun added in Linked List

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,9 +4,14 @@ using namespace std;
 void insert(){
     cout<<"Insert called";
 }
+void Delete(){
+    cout<<"Delete called";
+}
 int main()
 {
     cout<<"Hello";
     insert();
+    Delete();
+    
     return 0;
 }
